2bc578ffb3c9814309e5fb86bd01b51d45801ff4
TomRomeo <tom.romeo.doil@gmail.com>
2025-08-26 11:11:34 +0200

    feat: add nix flake
    fix: src path
    
    chore: disable homeManagerModules for now
    
    test
    
    fix
    
    fix
    
    fix
    
    fix
    
    try to remove derivation version
    
    fix
    
    fix
    
    fix
    
    fix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A flake for Vicinae, a high-performance native launcher for Linux.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  nixConfig = {
+    extra-substituters = ["https://tomromeo.cachix.org"];
+    extra-trusted-public-keys = ["tomromeo.cachix.org-1:RNtotxybU2sStsoulE4np6THs0bTw2SBtKFfKogBah0="];
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        vicinaePkg = pkgs.callPackage ./vicinae.nix { hash = self.rev ? self.dirtyRev; };
+      in
+      {
+        packages.default = vicinaePkg;
+      }
+    ) // {
+      homeManagerModules.default = {config,pkgs,lib,...}: import ./module.nix {inherit config pkgs lib self;};
+    };
+}
