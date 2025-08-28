b7f64392b65ab0c2f6bee3231158f90bde9f7db1
Aurelien Brabant <aurelien@brabant.dev>
2025-08-28 16:40:15 +0200

    fix(wlr-clip): refactor and destroy last offer on time

---
+++ wlr-clip/src/app.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <netinet/in.h>
+#include <memory>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <wayland-client-core.h>
+#include <wayland-client-protocol.h>
+#include <wayland-util.h>
+#include "data-control-client.hpp"
+#include "display.hpp"
+
+class Clipman : public WaylandDisplay, public WaylandRegistry::Listener, public DataDevice::Listener {
+
+public:
+  static Clipman *instance();
+  void start();
+  Clipman();
+
+private:
+  std::unique_ptr<WaylandRegistry> _registry;
+  std::unique_ptr<DataControlManager> _dcm;
+  std::unique_ptr<WaylandSeat> _seat;
+
+  void global(WaylandRegistry &reg, uint32_t name, const char *interface, uint32_t version) override;
+  void selection(DataDevice &device, DataOffer &offer) override;
+};
