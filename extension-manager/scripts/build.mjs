2b00e2b04d86805f3c96b50a0203a330240ae8fe
Aurelien Brabant <aurelien@brabant.dev>
2025-09-02 04:53:08 +0200

    feat: add dev mode for extension manager

--- extension-manager/scripts/build.mjs
+++ extension-manager/scripts/build.mjs
@@ -2,31 +2,35 @@ import * as esbuild from 'esbuild'
 import { mkdirSync, rmSync } from 'fs'
 import { join } from 'path';
 
-const outDir = join(import.meta.dirname, '..', 'dist');
-const outFile = join(outDir, 'runtime.js');
+export const build = async () => {
+	const outDir = join(import.meta.dirname, '..', 'dist');
+	const outFile = join(outDir, 'runtime.js');
 
-rmSync(outDir, { recursive: true, force: true });
-mkdirSync(outDir, { recursive: true });
+	rmSync(outDir, { recursive: true, force: true });
+	mkdirSync(outDir, { recursive: true });
 
-const start = performance.now();
+	const start = performance.now();
 
-console.log(`Building runtime...`);
+	console.log(`Building runtime...`);
 
-await esbuild.build({
-  entryPoints: ['src/index.ts'],
-  bundle: true,
-  outfile: outFile,
-  format: 'cjs',
-  external: [], 
-  minify: true,
-  platform: 'node',
-	alias: {
-		'@vicinae/api': '../api/src/', // we bundle the local api directly
-		'react': '../api/node_modules/react' // we want the react version specified in the api package
-	}
-})
+	await esbuild.build({
+	  entryPoints: ['src/index.ts'],
+	  bundle: true,
+	  outfile: outFile,
+	  format: 'cjs',
+	  external: [], 
+	  minify: true,
+	  platform: 'node',
+		alias: {
+			'@vicinae/api': '../api/src/', // we bundle the local api directly
+			'react': '../api/node_modules/react' // we want the react version specified in the api package
+		}
+	})
 
-const end = performance.now();
-const buildTimeMs = end - start;
+	const end = performance.now();
+	const buildTimeMs = end - start;
 
-console.log(`Runtime built at ${outFile} in ${Math.round(buildTimeMs)}ms 🚀`);
+	console.log(`Runtime built at ${outFile} in ${Math.round(buildTimeMs)}ms 🚀`);
+}
+
+build();
