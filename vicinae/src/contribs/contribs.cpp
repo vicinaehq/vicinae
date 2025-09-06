662681c85015be5aa2e280b36eba22155e71520f
Aurelien Brabant <aurelien@brabant.dev>
2025-09-06 17:47:41 +0200

    chore: bump to v0.9.0

--- vicinae/src/contribs/contribs.cpp
+++ vicinae/src/contribs/contribs.cpp
@@ -1,19 +1,21 @@
 #include "contribs.hpp"
 
 static std::vector<Contributor::Contributor> CONTRIB_LIST = {
-	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 916 },
-Contributor::Contributor{.login = "cilginc", .resource = ":contribs/cilginc", .contribs = 39 },
-Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 19 },
-Contributor::Contributor{.login = "quadratech188", .resource = ":contribs/quadratech188", .contribs = 19 },
+	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 969 },
+Contributor::Contributor{.login = "cilginc", .resource = ":contribs/cilginc", .contribs = 40 },
+Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 21 },
+Contributor::Contributor{.login = "quadratech188", .resource = ":contribs/quadratech188", .contribs = 20 },
 Contributor::Contributor{.login = "Damnjelly", .resource = ":contribs/Damnjelly", .contribs = 9 },
 Contributor::Contributor{.login = "ThatOneCalculator", .resource = ":contribs/ThatOneCalculator", .contribs = 8 },
 Contributor::Contributor{.login = "elliotnash", .resource = ":contribs/elliotnash", .contribs = 6 },
 Contributor::Contributor{.login = "RolfKoenders", .resource = ":contribs/RolfKoenders", .contribs = 4 },
-Contributor::Contributor{.login = "TomRomeo", .resource = ":contribs/TomRomeo", .contribs = 3 },
+Contributor::Contributor{.login = "TomRomeo", .resource = ":contribs/TomRomeo", .contribs = 4 },
 Contributor::Contributor{.login = "LuizSSampaio", .resource = ":contribs/LuizSSampaio", .contribs = 2 },
 Contributor::Contributor{.login = "juliogc", .resource = ":contribs/juliogc", .contribs = 2 },
 Contributor::Contributor{.login = "ArjixWasTaken", .resource = ":contribs/ArjixWasTaken", .contribs = 1 },
-Contributor::Contributor{.login = "UnknownCalculatedVariable", .resource = ":contribs/UnknownCalculatedVariable", .contribs = 1 }
+Contributor::Contributor{.login = "UnknownCalculatedVariable", .resource = ":contribs/UnknownCalculatedVariable", .contribs = 1 },
+Contributor::Contributor{.login = "dawsers", .resource = ":contribs/dawsers", .contribs = 1 },
+Contributor::Contributor{.login = "schromp", .resource = ":contribs/schromp", .contribs = 1 }
 };
 
 std::vector<Contributor::Contributor> Contributor::getList() { return CONTRIB_LIST; };
