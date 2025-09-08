88378751e4cf473aa907d2f6708ded9c0916b001
Aurelien Brabant <aurelien@brabant.dev>
2025-09-08 09:33:36 +0200

    feat: use specific package entrypoint for raycast extensions

--- extension-manager/src/patch-require.ts
+++ extension-manager/src/patch-require.ts
@@ -1,13 +1,10 @@
-import * as React from 'react';
-import * as vicinae from '@vicinae/api';
-import * as jsxRuntime from 'react/jsx-runtime';
 import Module from 'module';
 
 const requireOverrides: Record<string, any> = {
-	'react': React,
-	'react/jsx-runtime': jsxRuntime,
-	'@vicinae/api': vicinae,
-	'@raycast/api': vicinae
+	'react': require('react'),
+	'react/jsx-runtime': require('react/jsx-runtime'),
+	'@vicinae/api': require('@vicinae/api'),
+	'@raycast/api': require('@vicinae/api/raycast')
 };
 
 export const patchRequire = () => {
