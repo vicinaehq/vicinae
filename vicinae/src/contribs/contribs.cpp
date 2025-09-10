4597cb02f0f97c5eee48def44c3439830bf18a35
Aurelien Brabant <aurelien@brabant.dev>
2025-09-10 21:57:50 +0200

    chore: update contributor list

--- vicinae/src/contribs/contribs.cpp
+++ vicinae/src/contribs/contribs.cpp
@@ -1,18 +1,19 @@
 #include "contribs.hpp"
 
 static std::vector<Contributor::Contributor> CONTRIB_LIST = {
-	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 969 },
+	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 999 },
 Contributor::Contributor{.login = "cilginc", .resource = ":contribs/cilginc", .contribs = 40 },
-Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 21 },
+Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 22 },
 Contributor::Contributor{.login = "quadratech188", .resource = ":contribs/quadratech188", .contribs = 20 },
 Contributor::Contributor{.login = "Damnjelly", .resource = ":contribs/Damnjelly", .contribs = 9 },
 Contributor::Contributor{.login = "ThatOneCalculator", .resource = ":contribs/ThatOneCalculator", .contribs = 8 },
 Contributor::Contributor{.login = "elliotnash", .resource = ":contribs/elliotnash", .contribs = 6 },
+Contributor::Contributor{.login = "TomRomeo", .resource = ":contribs/TomRomeo", .contribs = 5 },
 Contributor::Contributor{.login = "RolfKoenders", .resource = ":contribs/RolfKoenders", .contribs = 4 },
-Contributor::Contributor{.login = "TomRomeo", .resource = ":contribs/TomRomeo", .contribs = 4 },
 Contributor::Contributor{.login = "LuizSSampaio", .resource = ":contribs/LuizSSampaio", .contribs = 2 },
 Contributor::Contributor{.login = "juliogc", .resource = ":contribs/juliogc", .contribs = 2 },
 Contributor::Contributor{.login = "ArjixWasTaken", .resource = ":contribs/ArjixWasTaken", .contribs = 1 },
+Contributor::Contributor{.login = "chmanie", .resource = ":contribs/chmanie", .contribs = 1 },
 Contributor::Contributor{.login = "UnknownCalculatedVariable", .resource = ":contribs/UnknownCalculatedVariable", .contribs = 1 },
 Contributor::Contributor{.login = "dawsers", .resource = ":contribs/dawsers", .contribs = 1 },
 Contributor::Contributor{.login = "schromp", .resource = ":contribs/schromp", .contribs = 1 }
