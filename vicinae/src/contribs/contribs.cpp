32a75417087e1c70b2f55276a5e753d34e825923
Aurelien Brabant <aurelien@brabant.dev>
2025-09-15 14:17:46 +0200

    chore: update contributor list

--- vicinae/src/contribs/contribs.cpp
+++ vicinae/src/contribs/contribs.cpp
@@ -1,11 +1,11 @@
 #include "contribs.hpp"
 
 static std::vector<Contributor::Contributor> CONTRIB_LIST = {
-	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 999 },
+	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 1025 },
 Contributor::Contributor{.login = "cilginc", .resource = ":contribs/cilginc", .contribs = 40 },
 Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 22 },
 Contributor::Contributor{.login = "quadratech188", .resource = ":contribs/quadratech188", .contribs = 20 },
-Contributor::Contributor{.login = "Damnjelly", .resource = ":contribs/Damnjelly", .contribs = 9 },
+Contributor::Contributor{.login = "Damnjelly", .resource = ":contribs/Damnjelly", .contribs = 10 },
 Contributor::Contributor{.login = "ThatOneCalculator", .resource = ":contribs/ThatOneCalculator", .contribs = 8 },
 Contributor::Contributor{.login = "elliotnash", .resource = ":contribs/elliotnash", .contribs = 6 },
 Contributor::Contributor{.login = "TomRomeo", .resource = ":contribs/TomRomeo", .contribs = 5 },
@@ -16,6 +16,7 @@ Contributor::Contributor{.login = "ArjixWasTaken", .resource = ":contribs/ArjixW
 Contributor::Contributor{.login = "chmanie", .resource = ":contribs/chmanie", .contribs = 1 },
 Contributor::Contributor{.login = "UnknownCalculatedVariable", .resource = ":contribs/UnknownCalculatedVariable", .contribs = 1 },
 Contributor::Contributor{.login = "dawsers", .resource = ":contribs/dawsers", .contribs = 1 },
+Contributor::Contributor{.login = "kaan-w", .resource = ":contribs/kaan-w", .contribs = 1 },
 Contributor::Contributor{.login = "schromp", .resource = ":contribs/schromp", .contribs = 1 }
 };
 
