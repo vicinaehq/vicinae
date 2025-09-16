16d4f720c62cfaa66b877fd0134d18c9dda4d042
Aurelien Brabant <aurelien@brabant.dev>
2025-09-16 16:29:21 +0200

    feat(extension): add support for more legacy Raycast APIs

---
+++ api/src/api/raycast/local-storage.ts
@@ -0,0 +1,15 @@
+import { LocalStorage } from '../local-storage';
+
+// We support deprecated local storage methods:
+// https://developers.raycast.com/misc/migration/v1.28.0#storage
+
+export const allLocalStorageItems = LocalStorage.allItems;
+export const getLocalStorageItem = LocalStorage.getItem;
+export const setLocalStorageItem = LocalStorage.setItem;
+export const removeLocalStorageItem = LocalStorage.removeItem;
+export const clearLocalStorage = LocalStorage.clear;
+
+export type LocalStorageValues = LocalStorage.Values;
+export type LocalStorageValue = LocalStorage.Value;
+
+export { LocalStorage } from '../local-storage';
