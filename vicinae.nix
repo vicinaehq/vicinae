2369c877e638e25efa91a977e3634e171eb938b5
Aurelien Brabant <aurelien@brabant.dev>
2025-09-26 19:23:55 +0200

    chore: update api nix hash

--- vicinae.nix
+++ vicinae.nix
@@ -45,7 +45,7 @@ let
   # Prepare node_modules for api folder
   apiDeps = fetchNpmDeps {
     src = src + /api;
-    hash = "sha256-WHZC0ckDtKIg6haSx/uTOTC5Q5GkxjnP0v3v/Nv05+Y=";
+    hash = "sha256-dSHEzw15lSRRbldl9PljuWFf2htdG+HgSeKPAB88RBg=";
   };
   ts-protoc-gen-wrapper = writeShellScriptBin "protoc-gen-ts_proto" ''
     exec node /build/source/vicinae-upstream/api/node_modules/.bin/protoc-gen-ts_proto
