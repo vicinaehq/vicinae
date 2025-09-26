096dbdf1a51a1b76db620bb4a4d268bc37be3335
Aurelien Brabant <aurelien@brabant.dev>
2025-09-26 16:12:04 +0200

    chore: update contributor list

--- vicinae/src/contribs/contribs.cpp
+++ vicinae/src/contribs/contribs.cpp
@@ -1,11 +1,11 @@
 #include "contribs.hpp"
 
 static std::vector<Contributor::Contributor> CONTRIB_LIST = {
-	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 1056 },
+	Contributor::Contributor{.login = "aurelien-brabant", .resource = ":contribs/aurelien-brabant", .contribs = 1066 },
 Contributor::Contributor{.login = "cilginc", .resource = ":contribs/cilginc", .contribs = 40 },
-Contributor::Contributor{.login = "quadratech188", .resource = ":contribs/quadratech188", .contribs = 23 },
-Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 22 },
-Contributor::Contributor{.login = "Damnjelly", .resource = ":contribs/Damnjelly", .contribs = 10 },
+Contributor::Contributor{.login = "quadratech188", .resource = ":contribs/quadratech188", .contribs = 24 },
+Contributor::Contributor{.login = "dagimg-dot", .resource = ":contribs/dagimg-dot", .contribs = 23 },
+Contributor::Contributor{.login = "Damnjelly", .resource = ":contribs/Damnjelly", .contribs = 11 },
 Contributor::Contributor{.login = "ThatOneCalculator", .resource = ":contribs/ThatOneCalculator", .contribs = 8 },
 Contributor::Contributor{.login = "elliotnash", .resource = ":contribs/elliotnash", .contribs = 6 },
 Contributor::Contributor{.login = "TomRomeo", .resource = ":contribs/TomRomeo", .contribs = 5 },
@@ -13,10 +13,11 @@ Contributor::Contributor{.login = "RolfKoenders", .resource = ":contribs/RolfKoe
 Contributor::Contributor{.login = "juliogc", .resource = ":contribs/juliogc", .contribs = 2 },
 Contributor::Contributor{.login = "schromp", .resource = ":contribs/schromp", .contribs = 2 },
 Contributor::Contributor{.login = "LuizSSampaio", .resource = ":contribs/LuizSSampaio", .contribs = 2 },
+Contributor::Contributor{.login = "koutselakismanos", .resource = ":contribs/koutselakismanos", .contribs = 2 },
 Contributor::Contributor{.login = "ArjixWasTaken", .resource = ":contribs/ArjixWasTaken", .contribs = 1 },
 Contributor::Contributor{.login = "bendi-github", .resource = ":contribs/bendi-github", .contribs = 1 },
 Contributor::Contributor{.login = "chmanie", .resource = ":contribs/chmanie", .contribs = 1 },
-Contributor::Contributor{.login = "koutselakismanos", .resource = ":contribs/koutselakismanos", .contribs = 1 },
+Contributor::Contributor{.login = "falood", .resource = ":contribs/falood", .contribs = 1 },
 Contributor::Contributor{.login = "soymadip", .resource = ":contribs/soymadip", .contribs = 1 },
 Contributor::Contributor{.login = "UnknownCalculatedVariable", .resource = ":contribs/UnknownCalculatedVariable", .contribs = 1 },
 Contributor::Contributor{.login = "dawsers", .resource = ":contribs/dawsers", .contribs = 1 },
