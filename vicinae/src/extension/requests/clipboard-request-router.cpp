3f4fb74af8fbef467ce54e6a57b5fa56ed267abf
Aurelien Brabant <aurelien@brabant.dev>
2025-10-03 00:50:07 +0200

    feat(api): implement clear and read clipboard APIs

--- vicinae/src/extension/requests/clipboard-request-router.cpp
+++ vicinae/src/extension/requests/clipboard-request-router.cpp
@@ -1,9 +1,12 @@
 #include "clipboard-request-router.hpp"
 #include "proto/clipboard.pb.h"
+#include <qclipboard.h>
+
+namespace clip_proto = proto::ext::clipboard;
 
 Clipboard::Content
-ClipboardRequestRouter::parseProtoClipboardContent(const proto::ext::clipboard::ClipboardContent &content) {
-  using ProtoContent = proto::ext::clipboard::ClipboardContent;
+ClipboardRequestRouter::parseProtoClipboardContent(const clip_proto::ClipboardContent &content) {
+  using ProtoContent = clip_proto::ClipboardContent;
 
   switch (content.content_case()) {
   case ProtoContent::kText:
@@ -22,40 +25,64 @@ ClipboardRequestRouter::parseProtoClipboardContent(const proto::ext::clipboard::
   return {};
 }
 
-proto::ext::clipboard::Response *
-ClipboardRequestRouter::paste(const proto::ext::clipboard::PasteToClipboardRequest &req) {
+clip_proto::Response *ClipboardRequestRouter::readContent(const clip_proto::ReadContentRequest &req) {
+  auto readContent = m_clipboard.readContent();
+  auto res = new clip_proto::Response;
+  auto data = new clip_proto::ReadContentResponse;
+  auto content = new clip_proto::ClipboardReadContent();
+
+  content->set_text(readContent.text.toStdString());
+
+  if (auto &html = readContent.html) { content->set_html(html->toStdString()); }
+  if (auto &file = readContent.file) { content->set_file(file->toStdString()); }
+
+  data->set_allocated_content(content);
+  res->set_allocated_read_content(data);
+
+  return res;
+}
+
+clip_proto::Response *ClipboardRequestRouter::clear(const clip_proto::ClearRequest &req) {
+  auto res = new clip_proto::Response;
+
+  QApplication::clipboard()->clear();
+  res->set_allocated_clear(new clip_proto::ClearResponse());
+
+  return res;
+}
+
+clip_proto::Response *ClipboardRequestRouter::paste(const clip_proto::PasteToClipboardRequest &req) {
   auto content = parseProtoClipboardContent(req.content());
 
   // wait for the window which might be closing
   QTimer::singleShot(100, this, [this, content]() { m_clipboard.pasteContent(content); });
 
-  auto resData = new proto::ext::clipboard::PasteToClipboardResponse;
-  auto res = new proto::ext::clipboard::Response;
+  auto resData = new clip_proto::PasteToClipboardResponse;
+  auto res = new clip_proto::Response;
 
   res->set_allocated_paste(resData);
 
   return res;
 }
 
-proto::ext::clipboard::Response *
-ClipboardRequestRouter::copy(const proto::ext::clipboard::CopyToClipboardRequest &req) {
+clip_proto::Response *ClipboardRequestRouter::copy(const clip_proto::CopyToClipboardRequest &req) {
   auto content = parseProtoClipboardContent(req.content());
   bool concealed = req.options().concealed();
 
   m_clipboard.copyContent(content, {.concealed = concealed});
 
-  auto resData = new proto::ext::clipboard::CopyToClipboardResponse;
-  auto res = new proto::ext::clipboard::Response;
+  auto resData = new clip_proto::CopyToClipboardResponse;
+  auto res = new clip_proto::Response;
 
   res->set_allocated_copy(resData);
 
   return res;
 }
 
-proto::ext::extension::Response *ClipboardRequestRouter::route(const proto::ext::clipboard::Request &req) {
-  namespace clipboard = proto::ext::clipboard;
+proto::ext::extension::Response *ClipboardRequestRouter::route(const clip_proto::Request &req) {
+  namespace clipboard = clip_proto;
 
-  auto wrap = [](proto::ext::clipboard::Response *clipRes) -> proto::ext::extension::Response * {
+  auto wrap = [](clip_proto::Response *clipRes) -> proto::ext::extension::Response * {
     auto res = new proto::ext::extension::Response;
     auto data = new proto::ext::extension::ResponseData;
 
@@ -69,6 +96,10 @@ proto::ext::extension::Response *ClipboardRequestRouter::route(const proto::ext:
     return wrap(copy(req.copy()));
   case clipboard::Request::kPaste:
     return wrap(paste(req.paste()));
+  case clipboard::Request::kReadContent:
+    return wrap(readContent(req.read_content()));
+  case clipboard::Request::kClear:
+    return wrap(clear(req.clear()));
   default:
     break;
   }
