3ea11cc57010190344415d1b605ff49dcae55397
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-10-03 19:19:59 +0300

    feat: track recently accessed files for all file actions, not just primary ones (#418)
    * feat: introduce FileActionWrapper to track file access for all file related actions, solves #394
    
    * refactor: remove FileActionWrapper and integrate file access tracking into OpenFileInAppAction and ignore copy actions from tracking

--- vicinae/src/actions/files/file-actions.hpp
+++ vicinae/src/actions/files/file-actions.hpp
@@ -18,3 +18,20 @@ public:
 private:
   std::filesystem::path m_path;
 };
+
+class OpenFileInAppAction : public OpenAppAction {
+public:
+  void execute(ApplicationContext *ctx) override {
+    auto files = ctx->services->fileService();
+
+    OpenAppAction::execute(ctx);
+    files->saveAccess(m_path);
+  }
+
+  OpenFileInAppAction(const std::filesystem::path &path, const std::shared_ptr<AbstractApplication> &app,
+                      const QString &title)
+      : OpenAppAction(app, title, {path.c_str()}), m_path(path) {}
+
+private:
+  std::filesystem::path m_path;
+};
