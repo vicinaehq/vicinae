931df8cbe6ceb45848d921e6bc23dad3e274c4c1
Aurelien Brabant <aurelien@brabant.dev>
2025-10-07 19:09:13 +0200

    refactor: raycast compatibility commands

--- vicinae/src/extensions/raycast/raycast-compat-extension.hpp
+++ vicinae/src/extensions/raycast/raycast-compat-extension.hpp
@@ -1,14 +1,14 @@
 #pragma once
-#include "common.hpp"
 #include "../../ui/image/url.hpp"
 #include "raycast-store-command.hpp"
+#include "theme.hpp"
 
 class RaycastCompatExtension : public BuiltinCommandRepository {
 public:
   QString id() const override { return "raycast-compat"; }
   QString displayName() const override { return "Raycast Compat"; }
   ImageURL iconUrl() const override {
-    return ImageURL::builtin("raycast").setBackgroundTint(Omnicast::ACCENT_COLOR);
+    return ImageURL::builtin("raycast").setBackgroundTint(SemanticColor::Red);
   }
   QString description() const override { return "Raycast compatiblity features"; }
 
--- vicinae/src/extensions/raycast/store/store-detail-view.hpp
+++ vicinae/src/extensions/raycast/store/store-detail-view.hpp
@@ -1,24 +1,13 @@
 #pragma once
 #include "actions/extension/extension-actions.hpp"
+#include "ui/vertical-scroll-area/vertical-scroll-area.hpp"
 #include "ui/views/base-view.hpp"
-#include "common.hpp"
-#include "ui/action-pannel/action.hpp"
 #include "ui/text-link/text-link.hpp"
 #include "ui/image/url.hpp"
 #include "services/raycast/raycast-store.hpp"
-#include "settings/extension-settings.hpp"
-#include "theme.hpp"
-#include "ui/image/image.hpp"
 #include "ui/list-accessory/list-accessory.hpp"
-#include "ui/scroll-bar/scroll-bar.hpp"
-#include "ui/thumbnail/thumbnail.hpp"
-#include "ui/typography/typography.hpp"
 #include "utils/layout.hpp"
-#include "utils/utils.hpp"
-#include "zip/unzip.hpp"
 #include <qboxlayout.h>
-#include "services/extension-registry/extension-registry.hpp"
-#include "services/toast/toast-service.hpp"
 #include <qevent.h>
 #include <qfuturewatcher.h>
 #include <qnamespace.h>
@@ -28,287 +17,30 @@
 #include <qurl.h>
 #include <qwidget.h>
 
-class ScreenshotList : public QScrollArea {
-  Q_OBJECT
-
-  QWidget *createWidget(const std::vector<QUrl> &urls) {
-    auto makeThumbnail = [this](const QUrl &url) -> Thumbnail * {
-      auto thumbnail = new Thumbnail;
-      double aspectRatio = 16 / 10.f;
-
-      connect(thumbnail, &Thumbnail::clicked, this, [this, url]() { emit clickedUrl(ImageURL::http(url)); });
-
-      thumbnail->setClickable(true);
-      thumbnail->setFixedHeight(150);
-      thumbnail->setFixedWidth(150 * aspectRatio);
-      thumbnail->setImage(ImageURL::http(url));
-
-      return thumbnail;
-    };
-
-    return HStack().map(urls, makeThumbnail).addStretch().spacing(10).buildWidget();
-  }
-
-  void setupUI() {
-    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setHorizontalScrollBar(new OmniScrollBar);
-  }
-
-public:
-  void setUrls(const std::vector<QUrl> &urls) {
-    setWidget(createWidget(urls));
-    setWidgetResizable(true);
-    setAutoFillBackground(true);
-  }
-
-  ScreenshotList() { setupUI(); }
-
-signals:
-  void clickedUrl(const ImageURL &url);
-};
-
 class RaycastStoreDetailView : public BaseView {
+public:
+  RaycastStoreDetailView(const Raycast::Extension &extension) : m_ext(extension) { setupUI(extension); }
+
+private:
   VerticalScrollArea *m_scrollArea = new VerticalScrollArea(this);
   Raycast::Extension m_ext;
   ListAccessoryWidget *m_installedAccessory = new ListAccessoryWidget;
 
-  bool supportsSearch() const override { return false; }
+  QString initialNavigationTitle() const override;
+  bool supportsSearch() const override;
+  Stack createContributorList();
 
-  Stack createContributorList() {
-    auto makeContributor = [&](const Raycast::User &user) {
-      return HStack().addIcon(user.validUserIcon().circle(), {16, 16}).addText(user.name).spacing(10);
-    };
-
-    return VStack().map(m_ext.contributors, makeContributor).spacing(10);
-  }
-
-  Stack createHeader() {
-    auto author = HStack()
-                      .addIcon(ImageURL::http(m_ext.author.avatar).circle(), {16, 16})
-                      .addText(m_ext.author.name)
-                      .spacing(10);
-    auto downloadCount = HStack()
-                             .addIcon(ImageURL::builtin("arrow-down-circle"), {16, 16})
-                             .addText(formatCount(m_ext.download_count))
-                             .spacing(5);
-
-    auto metadata = HStack()
-                        .add(author)
-                        .add(downloadCount)
-                        .addIf(!m_ext.platforms.isEmpty(),
-                               [&]() {
-                                 auto platforms = HStack().spacing(5);
-
-                                 if (m_ext.platforms.contains("macOS")) {
-                                   platforms.addIcon(ImageURL::builtin("apple"), {16, 16});
-                                 }
-                                 if (m_ext.platforms.contains("Windows")) {
-                                   platforms.addIcon(ImageURL::builtin("windows11"), {16, 16});
-                                 }
-
-                                 return platforms;
-                               })
-                        .addStretch()
-                        .divided(1)
-                        .spacing(10);
-
-    auto left = HStack()
-                    .addIcon(m_ext.themedIcon(), {64, 64})
-                    .add(VStack().addTitle(m_ext.title).add(metadata).margins(0, 4, 0, 4).justifyBetween())
-                    .spacing(20);
-
-    m_installedAccessory->setAccessory({
-        .text = "Installed",
-        .color = SemanticColor::Green,
-        .fillBackground = true,
-        .icon = ImageURL::builtin("check-circle"),
-    });
-
-    m_installedAccessory->setMaximumHeight(30);
-
-    return HStack().add(left).add(m_installedAccessory).justifyBetween();
-  }
-
-  Stack createMainWidget() {
-    return VStack()
-        .add(VStack()
-                 .addText("Description")
-                 .addParagraph(m_ext.description, SemanticColor::TextSecondary)
-                 .spacing(10))
-        .add(VStack()
-                 .addText("Commands", SemanticColor::TextSecondary)
-                 .add(VStack()
-                          .map(m_ext.commands,
-                               [&](const auto &cmd) {
-                                 return VStack()
-                                     .add(HStack()
-                                              .addIcon(cmd.themedIcon(), {20, 20})
-                                              .addText(cmd.title)
-                                              .spacing(10))
-                                     .addParagraph(cmd.description, SemanticColor::TextSecondary)
-                                     .spacing(10);
-                               })
-                          .divided(1)
-                          .spacing(15)
-
-                          )
-                 .spacing(20))
-        .addStretch()
-        .divided(1)
-        .margins(20)
-        .spacing(20);
-  }
-
-  // section with header and preview images
-  QWidget *createPresentationSection() {
-    auto header = createHeader().buildWidget();
-
-    header->setMaximumHeight(70);
-
-    return VStack()
-        .add(header)
-        .addIf(m_ext.metadata_count > 0,
-               [&]() {
-                 auto list = new ScreenshotList();
-                 list->setFixedHeight(160);
-                 list->setUrls(m_ext.screenshots());
-
-                 connect(list, &ScreenshotList::clickedUrl, this,
-                         &RaycastStoreDetailView::handleClickedScreenshot);
-
-                 return list;
-               })
-        .spacing(20)
-        .divided(1)
-        .margins(25)
-        .buildWidget();
-  }
-
-  QWidget *createSideMetadataSection() {
-    auto readmeLink = VStack()
-                          .addText("README", SemanticColor::TextSecondary)
-                          .add(new TextLinkWidget("Open README", QUrl(m_ext.readme_assets_path)))
-                          .spacing(5);
-    auto viewSource = VStack()
-                          .addText("Source Code", SemanticColor::TextSecondary)
-                          .add(new TextLinkWidget("View Code", QUrl(m_ext.source_url)))
-                          .spacing(5);
-
-    auto lastUpdate =
-        VStack()
-            .addText("Last update", SemanticColor::TextSecondary)
-            .addText(getRelativeTimeString(m_ext.updatedAtDateTime()), SemanticColor::TextPrimary)
-            .spacing(5);
-
-    return VStack()
-        .add(readmeLink)
-        .add(lastUpdate)
-        .addIf(!m_ext.contributors.isEmpty(),
-               [&]() {
-                 return VStack()
-                     .addText("Contributors", SemanticColor::TextSecondary)
-                     .add(createContributorList())
-                     .spacing(5);
-               })
-        .add(viewSource)
-        .addStretch()
-        .spacing(15)
-        .margins(10)
-        .buildWidget();
-  }
-
-  QWidget *createContentSection() {
-    return HStack()
-        .add(createMainWidget().buildWidget(), 2)
-        .add(createSideMetadataSection(), 1)
-        .divided(1)
-        .buildWidget();
-  }
-
-  void handleClickedScreenshot(const ImageURL &url) {
-    auto dialog = new ImagePreviewDialogWidget(url);
-
-    dialog->setAspectRatio(16 / 10.f);
-    context()->navigation->setDialog(dialog);
-  }
-
-  QWidget *createUI(const Raycast::Extension &ext) {
-    return VStack().add(createPresentationSection()).add(createContentSection()).divided(1).buildWidget();
-  }
+  Stack createHeader();
+  Stack createMainWidget();
+  QWidget *createPresentationSection();
+  QWidget *createSideMetadataSection();
+  QWidget *createContentSection();
+  void handleClickedScreenshot(const ImageURL &url);
+  QWidget *createUI(const Raycast::Extension &ext);
 
   void extensionInstalled() { m_installedAccessory->show(); }
-
   void extensionUninstalled() { m_installedAccessory->hide(); }
-
-  void createActions() {
-    auto panel = std::make_unique<ActionPanelState>();
-    auto install = new StaticAction(
-        "Install extension", m_ext.themedIcon(), [ext = m_ext](const ApplicationContext *ctx) {
-          using Watcher = QFutureWatcher<Raycast::DownloadExtensionResult>;
-          auto store = ctx->services->raycastStore();
-          auto watcher = new Watcher;
-          ctx->services->toastService()->setToast("Downloading extension...");
-
-          QObject::connect(watcher, &Watcher::finished, [ctx, ext, watcher]() {
-            auto result = watcher->result();
-
-            watcher->deleteLater();
-
-            if (!result) {
-              ctx->services->toastService()->setToast("Failed to download extension");
-              return;
-            }
-
-            ctx->services->extensionRegistry()->installFromZip(ext.id,
-                                                               std::string_view(result->toStdString()));
-            ctx->services->toastService()->setToast("Extension downloaded");
-          });
-
-          auto downloadResult = store->downloadExtension(ext.download_url);
-
-          watcher->setFuture(downloadResult);
-        });
-    auto uninstall = new UninstallExtensionAction(m_ext.id);
-
-    auto main = panel->createSection();
-    auto danger = panel->createSection();
-
-    main->addAction(install);
-    install->setPrimary(true);
-    danger->addAction(uninstall);
-    uninstall->setShortcut(Keybind::RemoveAction);
-
-    setActions(std::move(panel));
-  }
-
-  void initialize() override {
-    auto registry = context()->services->extensionRegistry();
-    bool isInstalled = registry->isInstalled(m_ext.id);
-
-    if (isInstalled) {
-      extensionInstalled();
-    } else {
-      extensionUninstalled();
-    }
-
-    createActions();
-    connect(registry, &ExtensionRegistry::extensionAdded, this, [this](const QString &id) {
-      if (id != m_ext.id) return;
-      extensionInstalled();
-    });
-
-    connect(registry, &ExtensionRegistry::extensionUninstalled, this, [this](const QString &id) {
-      if (id != m_ext.id) return;
-      extensionUninstalled();
-    });
-  }
-
-  void setupUI(const Raycast::Extension &extension) {
-    m_scrollArea->setWidget(createUI(extension));
-    VStack().add(m_scrollArea).imbue(this);
-  }
-
-public:
-  RaycastStoreDetailView(const Raycast::Extension &extension) : m_ext(extension) { setupUI(extension); }
+  void createActions();
+  void initialize() override;
+  void setupUI(const Raycast::Extension &extension);
 };
