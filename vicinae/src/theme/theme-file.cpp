ed5782aeaf9a0ef4136a8eee3a6efa9887f967ab
Aurelien Brabant <gh@aurelle.dev>
2025-10-12 22:37:57 +0200

    feat: add theme tint mappings

--- vicinae/src/theme/theme-file.cpp
+++ vicinae/src/theme/theme-file.cpp
@@ -1,29 +1,109 @@
 #include "theme-file.hpp"
 #include "lib/toml.hpp"
+#include <filesystem>
 #include <fstream>
 
 namespace fs = std::filesystem;
 
-static const std::array<const char *, 16> base16Keys = {
-    "base00", "base01", "base02", "base03", "base04", "base05", "base06", "base07",
-    "base08", "base09", "base0A", "base0B", "base0C", "base0D", "base0E", "base0F"};
+static const std::unordered_map<ThemeFileTint, const char *> tintToKey = {
+    {ThemeFileTint::Base00, "base00"}, {ThemeFileTint::Base01, "base01"}, {ThemeFileTint::Base02, "base02"},
+    {ThemeFileTint::Base03, "base03"}, {ThemeFileTint::Base04, "base04"}, {ThemeFileTint::Base05, "base05"},
+    {ThemeFileTint::Base06, "base06"}, {ThemeFileTint::Base07, "base07"}, {ThemeFileTint::Base08, "base08"},
+    {ThemeFileTint::Base09, "base09"}, {ThemeFileTint::Base0A, "base0A"}, {ThemeFileTint::Base0B, "base0B"},
+    {ThemeFileTint::Base0C, "base0C"}, {ThemeFileTint::Base0D, "base0D"}, {ThemeFileTint::Base0E, "base0E"},
+    {ThemeFileTint::Base0F, "base0F"},
 
-static const std::array<const char *, 8> base24Keys = {
-    "base10", "base11", "base12", "base13", "base14", "base15", "base16", "base17",
+    {ThemeFileTint::Base10, "base10"}, {ThemeFileTint::Base11, "base11"}, {ThemeFileTint::Base12, "base12"},
+    {ThemeFileTint::Base13, "base13"}, {ThemeFileTint::Base14, "base14"}, {ThemeFileTint::Base15, "base15"},
+    {ThemeFileTint::Base16, "base16"}, {ThemeFileTint::Base17, "base17"},
 };
 
+static const std::unordered_map<const char *, ThemeFileTint> keyToTint = {
+    {"base00", ThemeFileTint::Base00}, {"base01", ThemeFileTint::Base01}, {"base02", ThemeFileTint::Base02},
+    {"base03", ThemeFileTint::Base03}, {"base04", ThemeFileTint::Base04}, {"base05", ThemeFileTint::Base05},
+    {"base06", ThemeFileTint::Base06}, {"base07", ThemeFileTint::Base07}, {"base08", ThemeFileTint::Base08},
+    {"base09", ThemeFileTint::Base09}, {"base0A", ThemeFileTint::Base0A}, {"base0B", ThemeFileTint::Base0B},
+    {"base0C", ThemeFileTint::Base0C}, {"base0D", ThemeFileTint::Base0D}, {"base0E", ThemeFileTint::Base0E},
+    {"base0F", ThemeFileTint::Base0F}, {"base10", ThemeFileTint::Base10}, {"base11", ThemeFileTint::Base11},
+    {"base12", ThemeFileTint::Base12}, {"base13", ThemeFileTint::Base13}, {"base14", ThemeFileTint::Base14},
+    {"base15", ThemeFileTint::Base15}, {"base16", ThemeFileTint::Base16}, {"base17", ThemeFileTint::Base17},
+};
+
+namespace fs = std::filesystem;
+
+static const std::array<ThemeFileTint, 16> base16Keys = {
+    ThemeFileTint::Base00, ThemeFileTint::Base01, ThemeFileTint::Base02, ThemeFileTint::Base03,
+    ThemeFileTint::Base04, ThemeFileTint::Base05, ThemeFileTint::Base06, ThemeFileTint::Base07,
+    ThemeFileTint::Base08, ThemeFileTint::Base09, ThemeFileTint::Base0A, ThemeFileTint::Base0B,
+    ThemeFileTint::Base0C, ThemeFileTint::Base0D, ThemeFileTint::Base0E, ThemeFileTint::Base0F};
+
+static const std::array<ThemeFileTint, 8> base24Keys = {
+    ThemeFileTint::Base10, ThemeFileTint::Base11, ThemeFileTint::Base12, ThemeFileTint::Base13,
+    ThemeFileTint::Base14, ThemeFileTint::Base15, ThemeFileTint::Base16, ThemeFileTint::Base17};
+
+static QColor parseColor(toml::node_view<toml::node> node) {
+  if (auto str = node.as_string()) { return QColor(str->value_or("")); }
+
+  if (auto table = node.as_table()) {
+    QColor color;
+
+    if (auto name = (*table)["name"].as_string()) { color = name->value_or(""); }
+    if (auto opacity = (*table)["opacity"].as_floating_point()) { color.setAlphaF(opacity->value_or(1)); }
+
+    return color;
+  }
+
+  return QColor();
+}
+
 tl::expected<ThemeFile, QString> ThemeFile::fromFile(const fs::path &path) {
+  ThemeFile themeFile(path);
   auto file = toml::parse_file(path.c_str());
+
+  auto metaPtr = file["meta"].as_table();
+
+  if (!metaPtr) { return tl::unexpected("a [meta] table is required"); }
+
+  auto &meta = *metaPtr;
+
+  themeFile.m_name = QString::fromStdString(meta["name"].value<std::string>().value_or(""));
+  themeFile.m_description = QString::fromStdString(meta["description"].value<std::string>().value_or(""));
+
+  if (auto ptr = meta["icon"].as_string()) {
+    std::string icon = ptr->value_or("");
+    fs::path iconPath = icon;
+    std::error_code ec;
+
+    if (icon.starts_with('.')) { iconPath = path / icon; }
+    if (!fs::is_regular_file(icon, ec)) {
+      return tl::unexpected(QString("%1 does not point to a valid file").arg(iconPath.c_str()));
+    }
+  }
+
   auto tints = file["tints"].as_table();
 
   if (!tints) { return tl::unexpected("a [tints] table is required"); }
 
   for (const auto &key : base16Keys) {
-    auto value = (*tints)[key].as_string();
-    if (!value) return tl::unexpected(QString("tints.%1 is required").arg(key));
+    if (auto it = tintToKey.find(key); it != tintToKey.end()) {
+      auto value = (*tints)[it->second];
+      if (!tints->contains(it->second)) {
+        return tl::unexpected(QString("tints.%1 is required").arg(it->second));
+      }
+      QColor color = parseColor(value);
+
+      if (!color.isValid()) {
+        return tl::unexpected(QString("tints.%1 is not a valid color").arg(it->second));
+      }
+    }
   }
 
-  for (const auto &key : base24Keys) {}
+  for (const auto &key : base24Keys) {
+    if (auto it = tintToKey.find(key); it != tintToKey.end()) {
+      auto value = (*tints)[it->second].as_string();
+      if (!value) {}
+    }
+  }
 
   return ThemeFile(path);
 }
--- vicinae/src/theme/theme-file.hpp
+++ vicinae/src/theme/theme-file.hpp
@@ -7,12 +7,48 @@ struct ThemeFileParsingError {};
 
 struct Tints {};
 
+enum class ThemeFileTint {
+  Base00,
+  Base01,
+  Base02,
+  Base03,
+  Base04,
+  Base05,
+  Base06,
+  Base07,
+  Base08,
+  Base09,
+  Base0A,
+  Base0B,
+  Base0C,
+  Base0D,
+  Base0E,
+  Base0F,
+
+  Base10,
+  Base11,
+  Base12,
+  Base13,
+  Base14,
+  Base15,
+  Base16,
+  Base17
+};
+
 class ThemeFile {
 public:
   static tl::expected<ThemeFile, QString> fromFile(const std::filesystem::path &path);
 
+  QColor resolveTint(ThemeFileTint tint) const {
+    if (auto it = m_tints.find(tint); it != m_tints.end()) { return it->second; }
+    return QColor();
+  }
+
 private:
   ThemeFile(const std::filesystem::path &path);
 
-  Tints m_tints;
+  QString m_name;
+  QString m_description;
+  std::filesystem::path m_icon;
+  std::unordered_map<ThemeFileTint, QColor> m_tints;
 };
