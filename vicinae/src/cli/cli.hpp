e882cd22c25c78060291e477cdc820a778429bc1
Aurelle <gh@aurelle.dev>
2025-10-21 03:20:10 +0200

    feat: invoke the main vicinae binary to spawn the wlr clipboard server (#508)
    * feat: invoke the main vicinae binary to spawn the wlr clipboard server
    
    * refactor: do not compile wlr-clip as a separate library and move it under vicinae/

--- vicinae/src/cli/cli.hpp
+++ vicinae/src/cli/cli.hpp
@@ -1,9 +1,5 @@
 #pragma once
 #include "lib/CLI11.hpp"
-#include "lib/rang.hpp"
-#include <exception>
-#include <type_traits>
-#include "vicinae.hpp"
 
 class AbstractCommandLineCommand {
 public:
@@ -41,52 +37,8 @@ concept CliCommandType = std::is_base_of_v<AbstractCommandLineCommand, T>;
 class CommandLineApp {
 public:
   CommandLineApp(const std::string &name) : m_name(name) {}
-
   template <CliCommandType T> void registerCommand() { m_cmds.emplace_back(std::make_unique<T>()); }
-
-  int run(int ac, char **av) {
-    CLI::App app(m_name);
-
-    for (const auto &cmd : m_cmds) {
-      auto sub = app.add_subcommand(cmd->id(), cmd->description());
-      cmd->prepare(sub);
-      cmd->setup(sub);
-      sub->callback([cmd = cmd.get(), sub]() { cmd->run(sub); });
-    }
-
-    if (ac == 1) {
-      std::cout << app.help() << std::endl;
-      return 0;
-    }
-
-    // we still support direct deeplink usage
-    // i.e vicinae vicinae://extensions/vicinae/clipboard/history
-    if (ac == 2) {
-      QString arg = av[1];
-      auto pred = [&](const QString &scheme) { return arg.startsWith(scheme + "://"); };
-      bool hasScheme = std::ranges::any_of(Omnicast::APP_SCHEMES, pred);
-      if (hasScheme) {
-        char *subAv[] = {av[0], strdup("deeplink"), strdup(arg.toStdString().c_str()), nullptr};
-        return run(3, subAv);
-      }
-    }
-
-    app.require_subcommand();
-
-    std::atexit([]() { std::cout << rang::style::reset; });
-
-    try {
-      app.parse(ac, av);
-      return 0;
-    } catch (const CLI::ParseError &e) {
-      if (e.get_exit_code() != 0) { std::cerr << rang::fg::red; }
-      return app.exit(e);
-    } catch (const std::exception &except) {
-      std::cerr << rang::fg::red;
-      app.exit(CLI::Error("Exception", except.what()));
-      return 1;
-    }
-  }
+  int run(int ac, char **av);
 
 private:
   std::string m_name;
