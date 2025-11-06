fd02d31f24adf0c9e141cc65c83f9e0342afe7eb
Aurelien Brabant <gh@aurelle.dev>
2025-11-06 08:51:26 +0100

    fix: use proper length for oauth code verifier

--- typescript/api/src/api/oauth.ts
+++ typescript/api/src/api/oauth.ts
@@ -313,7 +313,7 @@ export class PKCEClient {
 	async authorizationRequest(
 		options: OAuth.AuthorizationRequestOptions,
 	): Promise<OAuth.AuthorizationRequest> {
-		const codeVerifier = randomBytes(128).toString("hex");
+		const codeVerifier = randomBytes(32).toString("base64url");
 		const codeChallenge = createHash("sha256")
 			.update(codeVerifier)
 			.digest("base64url");
