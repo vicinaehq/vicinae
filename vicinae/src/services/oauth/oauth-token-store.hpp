63fbdad07f599cb1326aaf05f9d2be845a02a700
Aurelien Brabant <gh@aurelle.dev>
2025-11-06 07:35:26 +0100

    fix: use explicit type in value_or

--- vicinae/src/services/oauth/oauth-token-store.hpp
+++ vicinae/src/services/oauth/oauth-token-store.hpp
@@ -20,7 +20,7 @@ struct TokenSet {
   std::vector<QString> scopes() const {
     return scope
         .transform([](const QString &scope) { return scope.split(',') | std::ranges::to<std::vector>(); })
-        .value_or({});
+        .value_or(std::vector<QString>());
   }
 
   QString extensionId;
