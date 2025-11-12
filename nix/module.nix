fced4592e5ab444ddfe3295d98c55a4832b5e266
Sir Aceroph <109217147+Aceroph@users.noreply.github.com>
2025-11-12 16:16:49 -0500

    fix (nix module): use TOML parser instead of JSON for themes (#675)
    * fix (nix module): use TOML parser instead of JSON for themes
    
    * fix (nix): Move generated theme files to XDG_DATA_HOME instead of XDG_CONFIG_HOME

--- nix/module.nix
+++ nix/module.nix
@@ -12,6 +12,7 @@ let
   vicinaePkg = self.packages.${system}.default;
 
   settingsFormat = pkgs.formats.json { };
+  themesFormat = pkgs.formats.toml { };
 in
 {
 
@@ -61,7 +62,7 @@ in
         valueType;
       default = { };
       description = ''
-        Theme settings to add to the themes folder in `~/.config/vicinae/themes`. 
+        Theme settings to add to the themes folder in `~/.local/share/vicinae/themes`. 
         The attribute name of the theme will be the name of theme json file, 
         e.g. `base16-default-dark` will be `base16-default-dark.json`.
       '';
@@ -135,24 +136,25 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = [ cfg.package ];
 
+    xdg.dataFile =
+      builtins.listToAttrs (
+        builtins.map (item: {
+          name = "vicinae/extensions/${item.name}";
+          value.source = item;
+        }) config.services.vicinae.extensions
+      )
+      // lib.mapAttrs' (
+        name: theme:
+        lib.nameValuePair "vicinae/themes/${name}.toml" {
+          source = themesFormat.generate "${name}.toml" theme;
+        }
+      ) cfg.themes;
+      
     xdg.configFile = {
       "vicinae/vicinae.json" = {
         source = settingsFormat.generate "vicinae.json" cfg.settings;
       };
-    }
-    // lib.mapAttrs' (
-      name: theme:
-      lib.nameValuePair "vicinae/themes/${name}.json" {
-        source = settingsFormat.generate "${name}.json" theme;
-      }
-    ) cfg.themes;
-
-    xdg.dataFile = builtins.listToAttrs (
-      builtins.map (item: {
-        name = "vicinae/extensions/${item.name}";
-        value.source = item;
-      }) config.services.vicinae.extensions
-    );
+    };
 
     systemd.user.services.vicinae = {
       Unit = {
