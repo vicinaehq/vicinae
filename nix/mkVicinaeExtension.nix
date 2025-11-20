b505e2cfee4edc73179c40e91f3b0db06b2e8d24
0xda157 <da157@voidq.com>
2025-11-19 22:54:33 -0800

    nix: move mkVicinaeExtension into packages, use callPackage

--- nix/mkVicinaeExtension.nix
+++ nix/mkVicinaeExtension.nix
@@ -1,13 +1,21 @@
 {
-  pkgs,
+  lib,
+  buildNpmPackage,
+  importNpmLock,
+}:
+{
   name,
   src,
+  # Keep to prevent breaking api and provide warning.
+  # TODO: throw after 26.05, remove after 26.11 NixOS release
+  pkgs ? null,
 }:
-pkgs.buildNpmPackage {
-  inherit name src;
-  npmDeps = pkgs.importNpmLock { npmRoot = src; };
-  npmConfigHook = pkgs.importNpmLock.npmConfigHook;
-  # NOTE: using buildPhase because $out doesn't work with npmBuildFlags
-  buildPhase = "npm run build -- --out=$out";
-  dontNpmInstall = true;
-}
+lib.warnIf (pkgs != null) "passing pkgs to mkVicinaeExtension is deprecated and no longer necessary"
+  (buildNpmPackage {
+    inherit name src;
+    npmDeps = importNpmLock { npmRoot = src; };
+    npmConfigHook = importNpmLock.npmConfigHook;
+    # NOTE: using buildPhase because $out doesn't work with npmBuildFlags
+    buildPhase = "npm run build -- --out=$out";
+    dontNpmInstall = true;
+  })
