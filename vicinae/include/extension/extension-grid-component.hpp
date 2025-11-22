6152ae10495ff0fd7fb37501a2584f902afee41f
Rithvik Vibhu <rithvikvibhu@gmail.com>
2025-11-22 23:33:03 +0530

    grid: add Grid.Dropdown search accessory

--- vicinae/include/extension/extension-grid-component.hpp
+++ vicinae/include/extension/extension-grid-component.hpp
@@ -5,6 +5,7 @@
 #include "extend/grid-model.hpp"
 #include "extension/extension-view.hpp"
 #include "ui/empty-view/empty-view.hpp"
+#include "ui/form/selector-input.hpp"
 #include "extension/extension-grid.hpp"
 
 class ExtensionGridComponent : public ExtensionSimpleView {
@@ -15,14 +16,25 @@ public:
   void handleDebouncedSearchNotification();
   void textChanged(const QString &text) override;
   bool inputFilter(QKeyEvent *event) override;
+  void initialize() override;
 
   ExtensionGridComponent();
 
 private:
+  void renderDropdown(const DropdownModel &dropdown);
+  void handleDropdownSelectionChanged(const SelectorInput::AbstractItem &item);
+  void handleDropdownSearchChanged(const QString &text);
+
+  QWidget *searchBarAccessory() const override { return m_selector; }
+
+private:
   EmptyViewWidget *m_emptyView = new EmptyViewWidget(this);
   QStackedWidget *m_content = new QStackedWidget(this);
+  SelectorInput *m_selector = new SelectorInput(this);
   GridModel _model;
   ExtensionGridList *m_list = new ExtensionGridList;
   bool _shouldResetSelection;
   QTimer *_debounce;
+  QTimer *m_dropdownDebounce = new QTimer(this);
+  bool m_dropdownShouldResetSelection = false;
 };
