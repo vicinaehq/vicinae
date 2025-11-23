b386547309270ae2b887ab849ca5479ac71b39d5
Aurelien Brabant <gh@aurelle.dev>
2025-11-23 18:00:33 +0100

    fix: ignore application/x-qt-image

--- vicinae/src/lib/wayland/mime.hpp
+++ vicinae/src/lib/wayland/mime.hpp
@@ -9,10 +9,15 @@ static const std::unordered_set<std::string_view> flagMimes = {
 static const std::unordered_set<std::string_view> dataMimes = {
 	"x-special/gnome-copied-files"
 };
+
+static const std::unordered_set<std::string_view> ignoredMimes = {
+	"application/x-qt-image" // we only pick one image format, we don't need this hint
+};
 // clang-format on
 
 namespace Wayland {
 inline bool isDataMime(std::string_view mime) {
+  if (ignoredMimes.contains(mime)) return false;
   return mime.starts_with("text/") || mime.starts_with("image/") || mime.starts_with("application/") ||
          dataMimes.contains(mime);
 }
