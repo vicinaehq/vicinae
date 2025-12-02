94a54a58282913eeaf0349feffdbc3ba0e29b9b2
Aurelien Brabant <gh@aurelle.dev>
2025-12-02 04:06:12 +0100

    fix: allow tab cycling in extension lists

--- vicinae/src/extension/extension-list.cpp
+++ vicinae/src/extension/extension-list.cpp
@@ -13,7 +13,7 @@ ExtensionList::ExtensionList() {
 
 bool ExtensionList::selectUp() { return m_list->selectUp(); }
 bool ExtensionList::selectDown() { return m_list->selectDown(); }
-void ExtensionList::selectNext() { /*return m_list->selectNext();*/ }
+void ExtensionList::selectNext() { return m_list->selectNext(); }
 void ExtensionList::activateCurrentSelection() const { m_list->activateCurrentSelection(); }
 
 bool ExtensionList::empty() const { return m_model->isEmpty(); }
