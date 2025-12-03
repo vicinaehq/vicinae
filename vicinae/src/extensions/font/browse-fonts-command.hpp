45c358253bc9546aca64a1e82a691ee4a0988e1c
Aurelle <gh@aurelle.dev>
2025-12-03 02:04:09 +0100

    refactor: migrate font browser to new API (#804)
    * refactor: migrate font browser to new API
    
    * feat: improve font search speed
    
    * refactor: remove trie
    
    * refactor: rename browse fonts command

--- vicinae/src/extensions/font/browse-fonts-command.hpp
+++ vicinae/src/extensions/font/browse-fonts-command.hpp
@@ -1,11 +1,11 @@
-#include "../../ui/image/url.hpp"
+#pragma once
+#include "browse/browse-fonts-view.hpp"
 #include "single-view-command-context.hpp"
-#include "theme.hpp"
-#include "vicinae/browse-fonts-view.hpp"
 
 class BrowseFontsCommand : public BuiltinViewCommand<BrowseFontsView> {
   QString id() const override { return "browse"; }
-  QString name() const override { return "Browse Fonts"; };
+  QString name() const override { return "Search Fonts"; };
+  std::vector<QString> keywords() const override { return {"browse fonts"}; }
   ImageURL iconUrl() const override {
     return ImageURL::builtin("text").setBackgroundTint(SemanticColor::Orange);
   }
