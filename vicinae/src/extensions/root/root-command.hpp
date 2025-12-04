f17771d53d2ef4e81a5c3492d656c07ec8fc08fb
Aurelien Brabant <gh@aurelle.dev>
2025-12-04 01:23:43 +0100

    refactor: remove old root search view

--- vicinae/src/extensions/root/root-command.hpp
+++ vicinae/src/extensions/root/root-command.hpp
@@ -1,8 +1,8 @@
 #pragma once
 #include "single-view-command-context.hpp"
-#include "root-search-view2.hpp"
+#include "root-search-view.hpp"
 
-class RootCommand : public BuiltinViewCommand<RootSearchView2> {
+class RootCommand : public BuiltinViewCommand<RootSearchView> {
   QString id() const override { return "root"; }
   QString name() const override { return ""; }
   ImageURL iconUrl() const override { return ImageURL::builtin("vicinae"); }
