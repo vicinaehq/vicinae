57f22ffd3cd961ba7dd4460b56423bb600c21b8c
Dagim G. Astatkie <workflow.jd@gmail.com>
2025-12-20 16:58:27 +0300

    fix: set raw image data for clipboard images, resolves #826 (#860)
    * fix: set raw image data for clipboard images, resolves #826
    
    - this is because Gnome D-Bust needs the raw data
    
    * refactor: get rid of extraneous comments
    
    ---------
    
    Co-authored-by: Aurelien Brabant <gh@aurelle.dev>

--- vicinae/src/services/clipboard/clipboard-service.cpp
+++ vicinae/src/services/clipboard/clipboard-service.cpp
@@ -540,7 +540,10 @@ bool ClipboardService::copySelection(const ClipboardSelection &selection,
         qWarning() << offer.mimeType << "could not be converted to valid image format";
         mimeData->setData(offer.mimeType, offer.data);
       } else {
+        mimeData->setData(offer.mimeType, offer.data);
         mimeData->setImageData(img);
+        qDebug() << "ClipboardService: Set image data with mime type" << offer.mimeType
+                 << "size:" << offer.data.size();
       }
     } else {
       if (Utils::isTextMimeType(offer.mimeType)) {
