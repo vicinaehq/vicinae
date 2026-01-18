78043c419d9326d8fd6e6b11c794a4ef8b105cac
Aurelle <gh@aurelle.dev>
2026-01-18 20:29:24 +0100

    fix: account for cancelled file searches (#976)

--- vicinae/src/extensions/file/search/file-search-model.hpp
+++ vicinae/src/extensions/file/search/file-search-model.hpp
@@ -1,14 +1,12 @@
 #pragma once
-#include "ui/default-list-item-widget/default-list-item-widget.hpp"
-#include "ui/vlist/common/section-model.hpp"
+#include "ui/vlist/common/vertical-list-model.hpp"
 #include "utils.hpp"
+#include <cstdint>
 
-enum class FileSearchModelSection { Results };
+enum class FileSearchModelSection : std::uint8_t { Results };
 
-class FileSearchModel : public vicinae::ui::SectionListModel<std::filesystem::path, FileSearchModelSection> {
+class FileSearchModel : public vicinae::ui::VerticalListModel<std::filesystem::path, FileSearchModelSection> {
 public:
-  FileSearchModel() {}
-
   void setSectionName(std::string_view name) { m_sectionName = name; }
 
   void setFiles(const std::vector<std::filesystem::path> &files) {
@@ -21,6 +19,7 @@ public:
   FileSearchModelSection sectionIdFromIndex(int idx) const override {
     return FileSearchModelSection::Results;
   }
+
   int sectionItemCount(FileSearchModelSection id) const override {
     switch (id) {
     case FileSearchModelSection::Results:
@@ -41,19 +40,14 @@ public:
   int sectionItemHeight(FileSearchModelSection id) const override { return 41; }
 
   StableID stableId(const std::filesystem::path &item) const override {
-    static std::hash<std::filesystem::path> hasher = {};
-    return hasher(item);
+    return std::hash<std::filesystem::path>{}(item);
   }
 
-  WidgetTag widgetTag(const std::filesystem::path &item) const override { return 1; }
-  WidgetType *createItemWidget(const std::filesystem::path &type) const override {
-    return new DefaultListItemWidget;
-  }
-  void refreshItemWidget(const std::filesystem::path &path, WidgetType *widget) const override {
-    auto w = static_cast<DefaultListItemWidget *>(widget);
-    w->setIconUrl(ImageURL::fileIcon(path));
-    w->setName(getLastPathComponent(path).c_str());
-    w->setActive(false);
+  ItemData createItemData(const std::filesystem::path &path) const override {
+    return ItemData{
+        .title = getLastPathComponent(path).c_str(),
+        .icon = ImageURL::fileIcon(path),
+    };
   }
 
 private:
