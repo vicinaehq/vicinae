2c9c4a6b0ddb4fc607e910df8d2dd881cbbe574c
0xda157 <da157@voidq.com>
2026-01-18 18:10:06 -0800

    mkRayCastExtension: use extendMkDerviation

--- nix/mkRayCastExtension.nix
+++ nix/mkRayCastExtension.nix
@@ -4,39 +4,39 @@
   importNpmLock,
   fetchgit,
 }:
-{
-  name,
-  sha256,
-  rev,
-}@args:
-let
-  src =
-    fetchgit {
-      inherit rev sha256;
-      url = "https://github.com/raycast/extensions";
-      sparseCheckout = [
-        "/extensions/${name}"
-      ];
-    }
-    + "/extensions/${name}";
-in
-buildNpmPackage (
-  {
-    inherit name src;
-    installPhase = ''
-      runHook preInstall
+lib.extendMkDerivation {
+  constructDrv = buildNpmPackage;
 
-      mkdir -p $out
-      cp -r /build/.config/raycast/extensions/${name}/* $out/
+  extendDrvArgs =
+    finalAttrs:
+    {
+      name,
+      rev,
+      sha256,
+      ...
+    }:
+    {
+      inherit name;
+      src =
+        fetchgit {
+          inherit rev sha256;
+          url = "https://github.com/raycast/extensions";
+          sparseCheckout = [
+            "/extensions/${name}"
+          ];
+        }
+        + "/extensions/${name}";
 
-      runHook postInstall
-    '';
-    npmDeps = importNpmLock { npmRoot = src; };
-    npmConfigHook = importNpmLock.npmConfigHook;
-  }
-  // builtins.removeAttrs args [
-    "name"
-    "sha256"
-    "rev"
-  ]
-)
+      npmDeps = importNpmLock { npmRoot = finalAttrs.src; };
+      npmConfigHook = importNpmLock.npmConfigHook;
+
+      installPhase = ''
+        runHook preInstall
+
+        mkdir -p $out
+        cp -r /build/.config/raycast/extensions/${name}/* $out/
+
+        runHook postInstall
+      '';
+    };
+}
