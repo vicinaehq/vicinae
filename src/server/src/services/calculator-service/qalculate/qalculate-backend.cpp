e8fd66e03f8ba7435639d488ccf84c6efd750e0b
lufimio <67525443+PA055@users.noreply.github.com>
2026-02-16 08:03:08 -0500

    Support for algebraic equation in qalculate backend (#1095)
    * feat: add support for algebraic calculations in qalculate
    
    * bug: i made a typo
    
    you can do integrate(3x, 5, 3) which wouldn't be indefinete so the comparison should also include 3

--- src/server/src/services/calculator-service/qalculate/qalculate-backend.cpp
+++ src/server/src/services/calculator-service/qalculate/qalculate-backend.cpp
@@ -7,8 +7,11 @@
 #include <libqalculate/includes.h>
 #include <libqalculate/Unit.h>
 #include <libqalculate/Prefix.h>
+#include <optional>
+#include <qcontainerfwd.h>
 #include <qlogging.h>
 #include <QtConcurrent/QtConcurrent>
+#include <qnamespace.h>
 #include <qobjectdefs.h>
 
 using CalculatorResult = QalculateBackend::CalculatorResult;
@@ -35,24 +38,23 @@ bool QalculateBackend::start() {
 
 std::expected<CalculatorResult, CalculatorError> QalculateBackend::compute(const QString &question) const {
   QString expression = preprocessQuestion(question);
-  std::string localizedExpression = CALCULATOR->unlocalizeExpression(expression.toStdString());
-  std::string res;
-  res = CALCULATOR->calculateAndPrint(localizedExpression, 10000, m_evalOpts, m_printOpts);
 
+  EvaluationOptions evalOpts = m_evalOpts;
+  PrintOptions printOpts = m_printOpts;
+
+  std::string localizedExpression = CALCULATOR->unlocalizeExpression(expression.toStdString());
+  std::string res = CALCULATOR->calculateAndPrint(localizedExpression, 10000, m_evalOpts, m_printOpts);
   MathStructure in = CALCULATOR->parse(localizedExpression);
   MathStructure result = CALCULATOR->parse(res);
-  if (result.containsUnknowns()) { return std::unexpected(CalculatorError("Unknown component in question")); }
 
-  bool error = false;
-
-  for (auto msg = CALCULATOR->message(); msg; msg = CALCULATOR->nextMessage()) {
-    error = true;
-  }
-
-  if (error) return std::unexpected(CalculatorError("Calculation error"));
+  if (auto err = drainMessages())
+    return std::unexpected(CalculatorError(err.value()));
 
   CalculatorResult calcRes;
-  if (result.containsType(STRUCT_UNIT)) {
+  calcRes.question.text = question;
+  calcRes.answer.text = QString::fromStdString(res);
+
+  if (result.containsType(STRUCT_UNIT) && !isIndefiniteIntegral(expression)) {
     if (auto unit = getUnitDisplayName(in)) { calcRes.question.unit = Unit{.displayName = unit->c_str()}; }
     if (auto unit = getUnitDisplayName(result)) { calcRes.answer.unit = Unit{.displayName = unit->c_str()}; }
     calcRes.type = CalculatorAnswerType::CONVERSION;
@@ -60,10 +62,63 @@ std::expected<CalculatorResult, CalculatorError> QalculateBackend::compute(const
     calcRes.type = CalculatorAnswerType::NORMAL;
   }
 
-  calcRes.question.text = question;
-  calcRes.answer.text = QString::fromStdString(res);
-
   return calcRes;
+}
+
+std::optional<QString> QalculateBackend::drainMessages() {
+  std::optional<QString> firstError;
+  for (auto *msg = CALCULATOR->message(); msg; msg = CALCULATOR->nextMessage()) {
+    switch (msg->type()) {
+      case MESSAGE_ERROR:
+        if (!firstError) firstError = QString::fromStdString(msg->message());
+        qWarning() << "[Qalculate] error:"   << msg->message().c_str();
+        break;
+      case MESSAGE_WARNING:
+        qWarning() << "[Qalculate] warning:" << msg->message().c_str();
+        break;
+      default:
+        qDebug()   << "[Qalculate] info:"    << msg->message().c_str();
+        break;
+    }
+  }
+  return firstError;
+}
+
+// I don't really like regex bashing to determine this but I couldn't
+// find any cleaner way to do it so I guess this works for now
+bool QalculateBackend::isIndefiniteIntegral(const QString &expr) {
+    const QString e = expr.simplified().toLower();
+
+    static const QRegularExpression integrateCall(
+        R"(\b(integrate|integral)\s*\()"
+    );
+
+    static const QRegularExpression integratePrefixOnly(
+        R"(\b(integrate|integral)\b(?!\s*\())"
+    );
+    if (integratePrefixOnly.match(e).hasMatch())
+        return true;
+
+    if (!integrateCall.match(e).hasMatch())
+        return false;
+
+    static const QRegularExpression outerArgs(
+        R"(\b(?:integrate|integral)\s*\((.+)\))"
+    );
+    const auto m = outerArgs.match(e);
+    if (!m.hasMatch())
+        return true;
+
+    // regex fails bc you can do smtn like "integral(f(x, y), y)" which should be indefinite
+    const QString args = m.captured(1);
+    int depth = 0, argCount = 1;
+    for (const QChar c : args) {
+        if (c == '(' || c == '[') ++depth;
+        else if (c == ')' || c == ']') --depth;
+        else if (c == ',' && depth == 0) ++argCount;
+    }
+
+    return argCount <= 3;
 }
 
 QString QalculateBackend::preprocessQuestion(const QString &query) const { return query.simplified(); }
@@ -98,17 +153,25 @@ QFuture<AbstractCalculatorBackend::RefreshExchangeRatesResult> QalculateBackend:
 bool QalculateBackend::supportsCurrencyConversion() const { return true; }
 
 void QalculateBackend::initializeCalculator() {
-  m_evalOpts.auto_post_conversion = POST_CONVERSION_BEST;
+  m_evalOpts = EvaluationOptions{};
+  m_evalOpts.auto_post_conversion = POST_CONVERSION_NONE;
+  m_evalOpts.auto_post_conversion = POST_CONVERSION_OPTIMAL;
+  m_evalOpts.do_polynomial_division = true;
   m_evalOpts.structuring = STRUCTURING_SIMPLIFY;
-  m_evalOpts.parse_options.limit_implicit_multiplication = true;
-  m_evalOpts.parse_options.parsing_mode = PARSING_MODE_CONVENTIONAL;
-  m_evalOpts.parse_options.units_enabled = true;
-  m_evalOpts.parse_options.unknowns_enabled = false;
+  m_evalOpts.parse_options.angle_unit = ANGLE_UNIT_DEGREES;
+  m_evalOpts.parse_options.limit_implicit_multiplication = false;
+  m_evalOpts.parse_options.parsing_mode = PARSING_MODE_ADAPTIVE;
+  m_evalOpts.parse_options.read_precision = DONT_READ_PRECISION;
 
-  m_printOpts.indicate_infinite_series = false;
-  m_printOpts.interval_display = INTERVAL_DISPLAY_SIGNIFICANT_DIGITS;
-  m_printOpts.use_unicode_signs = true;
+  m_printOpts = PrintOptions{};
   m_printOpts.base_display = BASE_DISPLAY_ALTERNATIVE;
+  m_printOpts.number_fraction_format = FRACTION_DECIMAL_EXACT;
+  m_printOpts.limit_implicit_multiplication = false;
+  m_printOpts.spacious = false;
+  m_printOpts.allow_factorization = true;
+  m_printOpts.interval_display = INTERVAL_DISPLAY_PLUSMINUS;
+  m_printOpts.use_unicode_signs = true;
+  m_printOpts.time_zone = TIME_ZONE_LOCAL;
 
   m_calc.reset();
   m_calc.loadGlobalDefinitions();
