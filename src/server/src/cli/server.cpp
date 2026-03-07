44b9badd90575c0b36a9a9c74d8b31e43df9aad2
Aurelien Brabant <gh@aurelle.dev>
2026-03-07 02:20:36 +0100

    feat: ai config

--- src/server/src/cli/server.cpp
+++ src/server/src/cli/server.cpp
@@ -155,14 +155,6 @@ int startServer(const ServerLaunchOptions &launchOpts) {
     registry->setTelemetry(std::make_unique<TelemetryService>(*registry->config()));
     registry->setAI(std::make_unique<AI::Service>());
 
-<<<<<<< HEAD
-=======
-    auto ollama = std::make_unique<AI::OllamaProvider>();
-
-    ollama->initalize({});
-    registry->ai()->registerProvider(std::move(ollama));
-
->>>>>>> 374be10e2 (feat: basic ollama integration)
     auto root = registry->rootItemManager();
     auto builtinCommandDb = std::make_unique<CommandDatabase>();
 
