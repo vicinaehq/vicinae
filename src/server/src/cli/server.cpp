3ab49c636a5991fbdc087576c92175c40b7c9ce1
Aurelien Brabant <gh@aurelle.dev>
2026-03-20 03:10:00 +0100

    feat: start working on agentic loop

--- src/server/src/cli/server.cpp
+++ src/server/src/cli/server.cpp
@@ -1,6 +1,8 @@
 #include "config/config.hpp"
 #include "environment.hpp"
+#include "services/ai/ai-tool.hpp"
 #include <QStyleHints>
+#include "services/ai/agentic-loop.hpp"
 #include "extension/extension.hpp"
 #include "root-search/browser-tabs/browser-tabs-provider.hpp"
 #include "root-search/scripts/script-root-provider.hpp"
@@ -52,6 +54,7 @@
 #include <QQuickWindow>
 #include <csignal>
 #include <QString>
+#include <glaze/json/prettify.hpp>
 #include <iostream>
 #include <qlockfile.h>
 #include <qlogging.h>
@@ -345,8 +348,18 @@ void CliServerCommand::run(CLI::App *) {
   QObject::connect(cfgService, &config::Manager::configChanged, configChanged);
   QIcon::setFallbackSearchPaths(Environment::fallbackIconSearchPaths());
 
+  AI::GenerateFunFact tool;
+
+  std::cout << "tool" << glz::prettify_json(tool.generateSchema()) << std::endl;
+
   auto builtinFont = ServiceRegistry::instance()->fontService()->builtinFontFamily();
   if (!builtinFont.isEmpty()) QGuiApplication::setFont(QFont(builtinFont));
+
+  QTimer::singleShot(2000, [&ctx]() {
+    auto agent = new AI::AgenticLoop(ctx);
+    qDebug() << "agent setup";
+    agent->addMessage("Tell me a fun fact about apples");
+  });
 
   configChanged(cfgService->value(), {});
 
