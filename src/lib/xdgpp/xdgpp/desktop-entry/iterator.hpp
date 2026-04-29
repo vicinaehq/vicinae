c08c69de2fc02b52775f3e152c0020eb5dae80f9
Aurelle <gh@aurelle.dev>
2026-04-29 20:02:16 +0200

    fix: crash on unreadable app directories (permission denied) (#1339)
    * fix: crash on unreadable app directories (permission denied)
    
    * chore: format

--- src/lib/xdgpp/xdgpp/desktop-entry/iterator.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "file.hpp"
-#include <vector>
-#include <filesystem>
-
-namespace xdgpp {
-std::vector<DesktopFile> getAllDesktopEntries();
-std::vector<DesktopFile> getAllDesktopEntries(const std::vector<std::filesystem::path> &paths);
-}; // namespace xdgpp
