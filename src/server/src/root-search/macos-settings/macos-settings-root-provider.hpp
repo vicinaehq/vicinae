04dcd9f6d21e09677bd6aafbb1f2ff005e0955a9
Aurelien Brabant <gh@aurelle.dev>
2026-05-24 19:14:39 +0200

    feat: macOS system settings provider
    Adds a root search provider to list macOS system settings.

---
+++ src/server/src/root-search/macos-settings/macos-settings-root-provider.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "services/root-item-manager/root-item-manager.hpp"
+#include <filesystem>
+
+struct MacSettingsPane {
+  std::filesystem::path bundlePath;
+  QString displayName;
+  QString bundleId;
+  QString legacyBundleId;
+};
+
+class MacSettingsRootItem : public RootItem {
+  MacSettingsPane m_pane;
+
+  QString title() const override;
+  QString typeDisplayName() const override;
+  ImageURL iconUrl() const override;
+  EntrypointId uniqueId() const override;
+  AccessoryList accessories() const override;
+  std::unique_ptr<ActionPanelState> newActionPanel(ApplicationContext *ctx,
+                                                   const RootItemMetadata &metadata) const override;
+  std::vector<std::pair<QString, QString>> settingsMetadata() const override;
+
+public:
+  explicit MacSettingsRootItem(MacSettingsPane pane) : m_pane(std::move(pane)) {}
+};
+
+class MacSettingsRootProvider : public RootProvider {
+public:
+  QString uniqueId() const override;
+  QString displayName() const override;
+  ImageURL icon() const override;
+  Type type() const override;
+  std::vector<std::shared_ptr<RootItem>> loadItems() const override;
+};
