dc1f7d47725c68debdb31be26781a96c14bac059
Aurelien Brabant <gh@aurelle.dev>
2026-05-26 23:56:41 +0200

    fix: do not advertise paste if input server is not running

--- src/server/src/services/paste/linux-paste-service.cpp
+++ src/server/src/services/paste/linux-paste-service.cpp
@@ -3,7 +3,9 @@
 
 LinuxPasteService::LinuxPasteService(LinuxInputServer &server) : m_server(server) {}
 
-bool LinuxPasteService::supportsPaste() const { return m_server.supportsKeyInjection(); }
+bool LinuxPasteService::supportsPaste() const {
+  return m_server.isRunning() && m_server.supportsKeyInjection();
+}
 
 bool LinuxPasteService::pasteToApp(const AbstractWindowManager::AbstractWindow *window,
                                    const AbstractApplication *app) {
