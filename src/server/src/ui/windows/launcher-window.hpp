2991934d44efad5a81ed4cf1799265396420f6eb
Aurelien Brabant <gh@aurelle.dev>
2026-05-29 15:50:18 +0200

    fix: compilation

--- src/server/src/ui/windows/launcher-window.hpp
+++ src/server/src/ui/windows/launcher-window.hpp
@@ -173,6 +173,7 @@ signals:
   void dragOverlayChanged();
   void dragActiveAnchorChanged();
   void filePickingChanged();
+  void rootSearchChanged();
 
 private:
   bool eventFilter(QObject *obj, QEvent *event) override;
