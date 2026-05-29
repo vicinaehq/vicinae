f57a994f071a28fe4bfe8debaf95f26fc7e80df2
Aurelien Brabant <gh@aurelle.dev>
2026-05-29 13:51:04 +0200

    refactor: disable notification example by default

--- extra/extension-boilerplate/src/desktop-notif-interval.ts
+++ extra/extension-boilerplate/src/desktop-notif-interval.ts
@@ -2,7 +2,7 @@ import { Color, Icon, sendDesktopNotification } from "@vicinae/api";
 
 export default async function DesktopNotificationInterval() {
 	await sendDesktopNotification({
-		title: "This is the title",
+		title: "Vicinae Extension Boilerplate Notification",
 		body: "This is the body of the notification, hopefully it displays correctly!",
 		icon: { source: Icon.Cog, tintColor: Color.Red },
 	});
