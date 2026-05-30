b049efc40adbd4162e62f5c4c52d9f53d33a6843
Aurelien Brabant <gh@aurelle.dev>
2026-05-30 17:31:04 +0200

    feat: populate environment.launchType with proper value
    Now that we support background execution of no-view commands with the
    `interval` manifest field, it is useful to be able to differenciate
    between foreground and background executions.
    
    So far `environment.launchType` was always set to user-initiated, which
    is no longer valid.

--- extra/extension-boilerplate/src/desktop-notif-interval.ts
+++ extra/extension-boilerplate/src/desktop-notif-interval.ts
@@ -1,9 +1,20 @@
-import { Color, Icon, sendDesktopNotification } from "@vicinae/api";
+import {
+	Color,
+	environment,
+	Icon,
+	LaunchType,
+	sendDesktopNotification,
+	showToast,
+} from "@vicinae/api";
 
 export default async function DesktopNotificationInterval() {
-	await sendDesktopNotification({
-		title: "Vicinae Extension Boilerplate Notification",
-		body: "This is the body of the notification, hopefully it displays correctly!",
-		icon: { source: Icon.Cog, tintColor: Color.Red },
-	});
+	if (environment.launchType === LaunchType.Background) {
+		await sendDesktopNotification({
+			title: "Vicinae Extension Boilerplate Notification",
+			body: "This is the body of the notification, hopefully it displays correctly!",
+			icon: { source: Icon.Cog, tintColor: Color.Red },
+		});
+	} else {
+		await showToast({ title: "Manually triggered, sent as a toast!" });
+	}
 }
