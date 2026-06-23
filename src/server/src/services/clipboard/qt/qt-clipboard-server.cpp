e78ac8fcdf9917c7dc3cd69f680e24a7d949c257
Signynt <vincenzo.barroso@gmail.com>
2026-06-23 15:55:32 +0200

    Fixed image support in clipboard for MacOS

--- src/server/src/services/clipboard/qt/qt-clipboard-server.cpp
+++ src/server/src/services/clipboard/qt/qt-clipboard-server.cpp
@@ -1,5 +1,57 @@
 #include "qt-clipboard-server.hpp"
+#include <QBuffer>
+#include <QImage>
+#include <QPixmap>
+#include <qlogging.h>
 #include <ranges>
+
+namespace {
+constexpr const char *CONCEALED_MARKER = "vicinae/concealed";
+constexpr const char *PASSWORD_MARKER = "x-kde-passwordManagerHint";
+
+std::optional<ClipboardDataOffer> imageOfferFromMimeData(const QMimeData *mimeData) {
+  auto formats = mimeData->formats();
+  QString selectedFormat;
+
+  if (formats.contains("image/png")) {
+    selectedFormat = "image/png";
+  } else if (formats.contains("image/jpeg")) {
+    selectedFormat = "image/jpeg";
+  } else if (formats.contains("image/jpg")) {
+    selectedFormat = "image/jpg";
+  } else if (formats.contains("image/svg+xml")) {
+    selectedFormat = "image/svg+xml";
+  } else {
+    for (const auto &format : formats) {
+      if (format.startsWith("image/")) {
+        selectedFormat = format;
+        break;
+      }
+    }
+  }
+
+  if (!selectedFormat.isEmpty()) {
+    return ClipboardDataOffer{.mimeType = selectedFormat, .data = mimeData->data(selectedFormat)};
+  }
+
+  QVariant imageData = mimeData->imageData();
+  QImage image = qvariant_cast<QImage>(imageData);
+  if (image.isNull()) {
+    auto pixmap = qvariant_cast<QPixmap>(imageData);
+    if (!pixmap.isNull()) { image = pixmap.toImage(); }
+  }
+
+  if (image.isNull()) return std::nullopt;
+
+  QByteArray encodedImage;
+  QBuffer buffer(&encodedImage);
+  if (!buffer.open(QIODevice::WriteOnly)) return std::nullopt;
+
+  if (!image.save(&buffer, "PNG")) return std::nullopt;
+
+  return ClipboardDataOffer{.mimeType = "image/png", .data = std::move(encodedImage)};
+}
+} // namespace
 
 bool AbstractQtClipboardServer::start() {
   auto clip = QGuiApplication::clipboard();
@@ -12,39 +64,26 @@ bool AbstractQtClipboardServer::stop() {
   return true;
 }
 
-void AbstractQtClipboardServer::dataChanged() {
+bool AbstractQtClipboardServer::setClipboardContent(QMimeData *data, const Clipboard::CopyOptions &options) {
+  if (options.concealed) { data->setData(CONCEALED_MARKER, "1"); }
+  return AbstractClipboardServer::setClipboardContent(data, options);
+}
+
+std::optional<ClipboardSelection>
+AbstractQtClipboardServer::selectionFromMimeData(const QMimeData *mimeData) {
+  if (!mimeData) return ClipboardSelection{};
+
+  if (mimeData->hasFormat(CONCEALED_MARKER)) {
+    qInfo() << "Qt clipboard: dropping concealed selection";
+    return std::nullopt;
+  }
+
   ClipboardSelection selection;
-  auto clip = QGuiApplication::clipboard();
-  auto mimeData = clip->mimeData();
+  selection.isPassword = mimeData->hasFormat(PASSWORD_MARKER);
 
   if (mimeData->hasImage()) {
-    // Prefer image formats in order: PNG > JPEG/JPG > SVG > anything else
-    auto formats = mimeData->formats();
-    QString selectedFormat;
-
-    if (formats.contains("image/png")) {
-      selectedFormat = "image/png";
-    } else if (formats.contains("image/jpeg")) {
-      selectedFormat = "image/jpeg";
-    } else if (formats.contains("image/jpg")) {
-      selectedFormat = "image/jpg";
-    } else if (formats.contains("image/svg+xml")) {
-      selectedFormat = "image/svg+xml";
-    } else {
-      // Find any other image format
-      for (const auto &format : formats) {
-        if (format.startsWith("image/")) {
-          selectedFormat = format;
-          break;
-        }
-      }
-    }
-
-    if (!selectedFormat.isEmpty()) {
-      ClipboardDataOffer offer;
-      offer.mimeType = selectedFormat;
-      offer.data = mimeData->data(selectedFormat);
-      selection.offers.emplace_back(offer);
+    if (auto imageOffer = imageOfferFromMimeData(mimeData)) {
+      selection.offers.emplace_back(std::move(*imageOffer));
     }
   }
 
@@ -71,7 +110,8 @@ void AbstractQtClipboardServer::dataChanged() {
   // We also want to index other formats that are not text, image, or legacy X11 target types.
 
   auto isIndexableFormat = [](const QString &fmt) {
-    return !isLegacyContentType(fmt) && !fmt.startsWith("text/") && !fmt.startsWith("image/");
+    return !isLegacyContentType(fmt) && !fmt.startsWith("text/") && !fmt.startsWith("image/") &&
+           fmt != PASSWORD_MARKER;
   };
 
   for (const auto &format : mimeData->formats() | std::views::filter(isIndexableFormat)) {
@@ -79,7 +119,13 @@ void AbstractQtClipboardServer::dataChanged() {
     selection.offers.emplace_back(ClipboardDataOffer{format, data});
   }
 
-  emit selectionAdded(selection);
+  return selection;
+}
+
+void AbstractQtClipboardServer::dataChanged() {
+  if (auto selection = selectionFromMimeData(QGuiApplication::clipboard()->mimeData())) {
+    emit selectionAdded(*selection);
+  }
 }
 
 bool AbstractQtClipboardServer::isLegacyContentType(const QString &str) {
