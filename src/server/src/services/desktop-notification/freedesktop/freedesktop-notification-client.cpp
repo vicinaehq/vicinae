6e50108f70bf9d74e758294419e5e0400f9a0a72
Aurelle <gh@aurelle.dev>
2026-07-06 23:07:42 +0200

    fix: do not use QDBusInterface for desktop notifications (#1618)

--- src/server/src/services/desktop-notification/freedesktop/freedesktop-notification-client.cpp
+++ src/server/src/services/desktop-notification/freedesktop/freedesktop-notification-client.cpp
@@ -1,28 +1,19 @@
 #include "freedesktop-notification-client.hpp"
 #include <QDBusConnection>
-#include <QDBusInterface>
 #include <QDBusMessage>
-#include <qcontainerfwd.h>
-#include <qlogging.h>
 
 // https://specifications.freedesktop.org/notification/latest/protocol.html
 
-FreedesktopNotificationClient::FreedesktopNotificationClient() {
-  m_iface = std::make_unique<QDBusInterface>(DBUS_SERVICE, DBUS_PATH, DBUS_INTERFACE,
-                                             QDBusConnection::sessionBus());
-}
-
 bool FreedesktopNotificationClient::send(const Notification &n) {
-  if (!m_iface->isValid()) return false;
-
   QVariantMap hints;
   hints[QStringLiteral("urgency")] = QVariant::fromValue(mapUrgency(n.urgency));
 
-  QDBusMessage reply =
-      m_iface->call(QStringLiteral("Notify"), QStringLiteral("Vicinae"), uint(0),
-                    n.iconPath.value_or(QString()), n.title, n.body, QStringList(), hints, -1);
+  QDBusMessage msg =
+      QDBusMessage::createMethodCall(DBUS_SERVICE, DBUS_PATH, DBUS_INTERFACE, QStringLiteral("Notify"));
+  msg << QStringLiteral("Vicinae") << uint(0) << n.iconPath.value_or(QString()) << n.title << n.body
+      << QStringList() << hints << -1;
 
-  return reply.type() != QDBusMessage::ErrorMessage;
+  return QDBusConnection::sessionBus().send(msg);
 }
 
 uchar FreedesktopNotificationClient::mapUrgency(Urgency urgency) {
