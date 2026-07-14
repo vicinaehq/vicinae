26344c9a6765a9414ae3a786399e6cb5d7515e85
Aurelien Brabant <gh@aurelle.dev>
2026-07-14 21:24:27 +0200

    refactor: remove already defined headers

--- src/server/src/services/clipboard/windows/windows-clipboard-server.cpp
+++ src/server/src/services/clipboard/windows/windows-clipboard-server.cpp
@@ -1,12 +1,6 @@
 ﻿#include <QTimer>
 #include <qendian.h>
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
 #include <windows.h>
 #include <appmodel.h>
 
