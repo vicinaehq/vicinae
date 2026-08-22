608bafe7bbea5b559a9a7a3c5c6acea6348fa081
Aurelien Brabant <gh@aurelle.dev>
2026-08-22 17:48:40 +0200

    feat: bump number version and fix unit formatting

--- src/server/src/services/calculator-service/numen/numen-calculator-backend.hpp
+++ src/server/src/services/calculator-service/numen/numen-calculator-backend.hpp
@@ -2,6 +2,29 @@
 #include "services/calculator-service/abstract-calculator-backend.hpp"
 #include "numen/numen.hpp"
 #include "services/calculator-service/numen/numen-currency-provider.hpp"
+#include <chrono>
+#include <format>
+
+namespace {
+std::string formatTimezone(const numen::Timezone &tz) {
+  std::string str = tz.toString();
+
+  if (tz.isUser()) {
+    str += " (Your time)";
+  } else if (tz.offset.count() == 0) {
+    const auto &info = tz.tz->get_info(std::chrono::system_clock::now());
+    auto secs = info.offset;
+    auto hours = std::chrono::floor<std::chrono::hours>(secs);
+    auto minutes = std::chrono::floor<std::chrono::minutes>(secs - hours);
+
+    str += std::format(" (GMT{:+}", hours.count());
+    if (minutes.count()) str += std::format(":{}", minutes.count());
+    str += ")";
+  }
+
+  return str;
+}
+}; // namespace
 
 class NumenCalculatorBackend : public AbstractCalculatorBackend {
 public:
@@ -28,9 +51,9 @@ public:
 
             if (auto c = res.conversion->as<numen::Timezone>()) {
               if (auto from = c->from; from) {
-                result.question.unit = Unit{.displayName = QString::fromStdString(from->toString())};
+                result.question.unit = Unit{.displayName = QString::fromStdString(formatTimezone(*from))};
               }
-              result.answer.unit = Unit{.displayName = QString::fromStdString(c->to.toString())};
+              result.answer.unit = Unit{.displayName = QString::fromStdString(formatTimezone(c->to))};
             }
           }
 
@@ -42,12 +65,18 @@ public:
                 if (n.unit && n.unit->resolved) {
                   result.answer.text = QString::fromStdString(n.text) + " " +
                                        QString::fromStdString(n.unit->resolved->render());
+                  if (!result.answer.unit) {
+                    result.answer.unit =
+                        Unit{.displayName = QString::fromStdString(n.unit->resolved->name())};
+                  }
                 }
               },
               [&](const numen::DateTime &dt) {
-                QString name =
-                    dt.isCurrentTimezone() ? "Your time" : QString::fromStdString(dt.toTimezoneString());
                 result.answer.text = QString::fromStdString(dt.toString({.withTz = false}));
+                if (!result.answer.unit) {
+                  numen::Timezone tz{.tz = dt.tz, .offset = dt.offset};
+                  result.answer.unit = Unit{.displayName = QString::fromStdString(formatTimezone(tz))};
+                }
               },
               [&](const numen::Boolean &b) { result.answer.text = b.value ? "true" : "false"; },
               [&](const numen::Duration &b) { result.answer.text = QString::fromStdString(b.toString()); }};
