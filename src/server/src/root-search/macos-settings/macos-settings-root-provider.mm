3d1cd82bec051532cdd1ae5ee3d55f66159533a0
Aurelien Brabant <gh@aurelle.dev>
2026-08-25 01:45:42 +0200

    fix: format

--- src/server/src/root-search/macos-settings/macos-settings-root-provider.mm
+++ src/server/src/root-search/macos-settings/macos-settings-root-provider.mm
@@ -165,7 +165,8 @@ MacSettingsRootItem::newActionPanel(ApplicationContext *ctx, const RootItemMetad
   utils->addAction(new CopyToClipboardAction(Clipboard::Text(url), tr("Copy URL")));
   utils->addAction(new CopyToClipboardAction(Clipboard::Text(m_pane.bundleId), tr("Copy Bundle ID")));
 
-  for (const auto &action : RootSearchActionGenerator::generateActions(*this, *ctx->services->rootItemManager())) {
+  for (const auto &action :
+       RootSearchActionGenerator::generateActions(*this, *ctx->services->rootItemManager())) {
     itemSection->addAction(action);
   }
 
