561b92b9ab961d20e4634bd11ef637c158174be3
Aurelien Brabant <gh@aurelle.dev>
2026-08-25 19:28:19 +0200

    fix: do not honor close on focus loss while dragging

--- src/server/src/qml/launcher-window.cpp
+++ src/server/src/qml/launcher-window.cpp
@@ -404,7 +404,7 @@ bool LauncherWindow::eventFilter(QObject *obj, QEvent *event) {
 
   // only works on some compositors.
   // we could probably make it work everywhere layer shell is supported by adding a layer behind ours.
-  else if (event->type() == QEvent::MouseMove && m_closeOnFocusLoss) {
+  else if (event->type() == QEvent::MouseMove && m_closeOnFocusLoss && !m_dragOverlayVisible) {
     auto *me = static_cast<QMouseEvent *>(event); // NOLINT
     QRect const contentRect(0, 0, m_window->width(), m_window->height());
     if (!contentRect.contains(me->position().toPoint())) { m_ctx.navigation->closeWindow(); }
