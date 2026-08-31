cc6a9dada266d61e53b7e67b1ab645724d69148d
Aurelien Brabant <gh@aurelle.dev>
2026-08-31 16:32:44 +0200

    feat: change default hotkey to alt+space on linux as well

--- src/server/src/config/config.hpp
+++ src/server/src/config/config.hpp
@@ -279,11 +279,7 @@ template <> struct Partial<InputServer> {
 };
 
 struct GlobalShortcuts {
-#if defined(Q_OS_MACOS) || defined(Q_OS_WIN)
   std::optional<std::string> toggle = "alt+space";
-#else
-  std::optional<std::string> toggle = "super+control+space";
-#endif
   std::vector<std::string> inhibitApps;
 };
 
