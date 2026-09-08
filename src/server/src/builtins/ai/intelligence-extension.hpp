dac27595a58d67ce8bf68a428f62e4ccb4c01865
Aurelien Brabant <gh@aurelle.dev>
2026-09-08 16:35:31 +0200

    feat: compile whisper with vulkan support on linux, and add nix support

--- src/server/src/builtins/ai/intelligence-extension.hpp
+++ src/server/src/builtins/ai/intelligence-extension.hpp
@@ -3,13 +3,14 @@
 #include "command/command-database.hpp"
 #include "command/single-view-command-context.hpp"
 #include "builtins/ai/transcribe-view-host.hpp"
+#include "theme/colors.hpp"
 #include "ui/image/url.hpp"
 
 class TranscribeCommand : public BuiltinViewCommand<TranscribeViewHost> {
   QString id() const override { return "transcribe"; }
   QString name() const override { return "Transcribe"; }
   ImageURL iconUrl() const override {
-    return ImageURL::builtin(BuiltinIcon::Microphone).setBackgroundTint(Omnicast::ACCENT_COLOR);
+    return ImageURL::builtin(BuiltinIcon::Microphone).setBackgroundTint(SemanticColor::Blue);
   }
   std::vector<QString> keywords() const override { return {"dictate"}; }
 };
