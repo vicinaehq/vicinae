caf3e00202c42acea615cb01fff0204e7de82b8e
Aurelien Brabant <gh@aurelle.dev>
2026-09-10 21:04:17 +0200

    chore: cleaner app platform iface

--- src/server/src/app-platform.hpp
+++ src/server/src/app-platform.hpp
@@ -1,16 +1,10 @@
 #pragma once
-#include <QtGlobal>
 
 namespace AppPlatform {
 
-#ifdef Q_OS_MACOS
 // Before QGuiApplication: Qt's Cocoa input context initialises TSM, which must happen in a UI app
 // or the panel never gets typing focus (Caps Lock layout switching).
 void beforeGuiApplication();
 void afterGuiApplication();
-#else
-inline void beforeGuiApplication() {}
-inline void afterGuiApplication() {}
-#endif
 
 } // namespace AppPlatform
