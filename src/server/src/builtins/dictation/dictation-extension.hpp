da9297ebc55fb449ee270251c43436ee44da9736
Aurelien Brabant <gh@aurelle.dev>
2026-09-11 05:43:25 +0200

    feat: add more transcription command boilerplate

--- src/server/src/builtins/dictation/dictation-extension.hpp
+++ src/server/src/builtins/dictation/dictation-extension.hpp
@@ -5,9 +5,17 @@
 #include "builtins/dictation/transcribe-view-host.hpp"
 #include "theme/colors.hpp"
 #include "ui/image/url.hpp"
+#include "services/toast/toast-service.hpp"
 
 namespace {
-const auto DICTATION_ICON = ImageURL::builtin(BuiltinIcon::Microphone).setBackgroundTint(SemanticColor::Blue);
+const auto COLOR = SemanticColor::Blue;
+const auto DICTATION_ICON = ImageURL::builtin(BuiltinIcon::Microphone).setBackgroundTint(COLOR);
+
+class UnimplementedCommand : public BuiltinCallbackCommand {
+  void execute(CommandController &controller) const override {
+    controller.context()->services->toastService()->failure("Not implemented");
+  }
+};
 
 class TranscribeCommand : public BuiltinViewCommand<TranscribeViewHost> {
   Q_DECLARE_TR_FUNCTIONS(TranscribeCommand)
@@ -17,6 +25,42 @@ class TranscribeCommand : public BuiltinViewCommand<TranscribeViewHost> {
   ImageURL iconUrl() const override { return DICTATION_ICON; }
   std::vector<QString> keywords() const override { return {"dictate"}; }
 };
+
+class AddVocabCommand : public UnimplementedCommand {
+  Q_DECLARE_TR_FUNCTIONS(AddVocabCommand)
+
+  QString id() const override { return "add-vocab"; }
+  QString name() const override { return tr("Add Word to Vocabulary"); }
+  ImageURL iconUrl() const override {
+    return ImageURL::builtin(BuiltinIcon::BookAntique).setBackgroundTint(COLOR).setBadge(BuiltinIcon::Plus);
+  }
+  std::vector<QString> keywords() const override { return {}; }
+};
+
+class DictationHistoryCommand : public UnimplementedCommand {
+  Q_DECLARE_TR_FUNCTIONS(DictationHistoryCommand)
+
+  QString id() const override { return "history"; }
+  QString name() const override { return tr("Dictation History"); }
+  ImageURL iconUrl() const override {
+    return ImageURL::builtin(BuiltinIcon::Microphone)
+        .setBackgroundTint(COLOR)
+        .setBadge(BuiltinIcon::MagnifyingGlass);
+  }
+  std::vector<QString> keywords() const override { return {}; }
+};
+
+class VocabularyCommand : public UnimplementedCommand {
+  Q_DECLARE_TR_FUNCTIONS(VocabularyCommand)
+
+  QString id() const override { return "vocab"; }
+  QString name() const override { return tr("Vocabulary"); }
+  ImageURL iconUrl() const override {
+    return ImageURL::builtin(BuiltinIcon::BookAntique).setBackgroundTint(COLOR);
+  }
+  std::vector<QString> keywords() const override { return {}; }
+};
+
 }; // namespace
 
 class DictationExtension : public BuiltinCommandRepository {
@@ -27,5 +71,10 @@ class DictationExtension : public BuiltinCommandRepository {
   ImageURL iconUrl() const override { return DICTATION_ICON; }
 
 public:
-  DictationExtension() { registerCommand<TranscribeCommand>(); }
+  DictationExtension() {
+    registerCommand<TranscribeCommand>();
+    registerCommand<VocabularyCommand>();
+    registerCommand<AddVocabCommand>();
+    registerCommand<DictationHistoryCommand>();
+  }
 };
