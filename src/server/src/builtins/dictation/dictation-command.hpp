f54e42d6c6c2d0f9451f2e2dd31af56aee3aa242
Aurelien Brabant <gh@aurelle.dev>
2026-09-13 04:29:59 +0200

    refactor: rename transcribe to dictation

--- src/server/src/builtins/dictation/dictation-command.hpp
+++ src/server/src/builtins/dictation/dictation-command.hpp
@@ -8,10 +8,10 @@
 class TranscribeCommand : public BuiltinCallbackCommand {
   Q_DECLARE_TR_FUNCTIONS(TranscribeCommand)
 
-  QString id() const override { return "transcribe"; }
-  QString name() const override { return tr("Transcribe"); }
+  QString id() const override { return "dictate"; }
+  QString name() const override { return tr("Dictate"); }
   ImageURL iconUrl() const override;
-  std::vector<QString> keywords() const override { return {"dictate"}; }
+  std::vector<QString> keywords() const override { return {"transcribe"}; }
 
   void execute(CommandController &controller) const override;
   void shortcutReleased() const override;
